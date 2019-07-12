a045c4d92dfa00f241189d416f36afe4204a6fbb
jeffery.raimundo@medaire.com <jeff_pepper23@yahoo.com>
2019-07-12 12:15:37 -0700

    routes outlined

--- server.js
+++ server.js
@@ -1,11 +1,12 @@
 const express = require("express");
-
+const routes = require("./routes");
 
 const app = express();
 const PORT = process.env.PORT || 3001;
 
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
+app.use(routes);
 
 if (process.env.NODE_ENV === "production") {
   app.use(express.static("client/build"));
