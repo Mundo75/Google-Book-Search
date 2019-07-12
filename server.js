12ed23df9cbcc0eb41e8ebd7f456f91607adc64f
jeffery.raimundo@medaire.com <jeff_pepper23@yahoo.com>
2019-07-12 16:34:06 -0700

    working app

--- server.js
+++ server.js
@@ -7,7 +7,7 @@ const PORT = process.env.PORT || 3001;
 
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
-app.use(express.static("public/build"));
+app.use(express.static("client/public"));
 app.use(routes);
 
 
